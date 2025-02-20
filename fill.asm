35925c85d0975f99fc4e1a489ae2bb67e52eb631
Mikhail <mckaylbing@gmail.com>
2025-02-20 10:18:54 -0700

    First version of pc memory

--- fill.asm
+++ fill.asm
@@ -1,44 +1,73 @@
 (BEGIN)
-	// Set keyboard to address for keyboard input value.
-	@24576
-	D=A
-	@keyboard
-	M=D
-(CHECK_KEYBOARD)
-	// Set current to last screen pixel map.
-	@24575
-	D=A
-	@current
-	M=D
-	// If keyboard is pressed, fill the screen.
-	@keyboard
-	A=M
-	D=M
-	@fillvalue
-	M=-1
-	@DRAW
-	D;JNE
-	// Otherwise, clear the screen.
-	@fillvalue
-	M=0
+        // Set keyboard to address for keyboard input value.
+        @24576
+        D=A
+        @keyboard
+        M=D
+
+(MAIN_LOOP)
+        // Fill the screen with black.
+        @fillvalue
+        M=-1
+        @FILL_SCREEN
+        0;JMP
+
+(FILL_SCREEN)
+        // Set current to last screen pixel map.
+        @24575
+        D=A
+        @current
+        M=D
+
 (DRAW)
-	// Fill or clear current pixel, depending on fillvalue.
-	@fillvalue
-	D=M
-	@current
-	A=M
-	M=D
-	// If current pixel map is first pixel map there is nothing left to draw, so
-	//jump back to keyboard check.
-	@current
-	D=M
-	@16384
-	D=D-A
-	@CHECK_KEYBOARD
-	D;JLE
-	// Decrement current pixel map.
-	@current
-	M=M-1
-	// Continue drawing next pixel map.
-	@DRAW
-	0;JMP
+        // Fill or clear current pixel, depending on fillvalue.
+        @fillvalue
+        D=M
+        @current
+        A=M
+        M=D
+
+        // If current pixel map is first pixel map, jump to DELAY.
+        @current
+        D=M
+        @16384
+        D=D-A
+        @DELAY
+        D;JLE
+
+        // Decrement current pixel map.
+        @current
+        M=M-1
+
+        // Continue drawing next pixel map.
+        @DRAW
+        0;JMP
+
+(DELAY)
+        // Delay loop to slow down the screen updates.
+        @delay_counter
+        M=0
+(DELAY_LOOP)
+        @delay_counter
+        M=M+1
+        D=M
+        @10000              // Adjust this value for faster/slower delay.
+        D=D-A
+        @DELAY_LOOP
+        D;JLT
+
+        // Toggle fillvalue between black and white.
+        @fillvalue
+        D=M
+        @TOGGLE_WHITE
+        D;JNE
+(TOGGLE_BLACK)
+        @fillvalue
+        M=-1
+        @MAIN_LOOP
+        0;JMP
+(TOGGLE_WHITE)
+        @fillvalue
+        M=0
+        @MAIN_LOOP
+        0;JMP
